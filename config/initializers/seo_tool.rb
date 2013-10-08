dbf897f0dc97ac672156d9dc9dc4bfbcb7f709fe
Adria Cidre <adria.cidre@gmail.com>
2013-10-08 15:57:46 +0200

    Removed env test

--- config/initializers/seo_tool.rb
+++
@@ -1,5 +0,0 @@
-if ['production', 'development'].include? Rails.env
-  raise 'Env variable oauth_id not setted' unless ENV['OAUTH_ID']
-  raise 'Env variable oauth_secret not setted' unless ENV['OAUTH_SECRET']
-  raise 'Env variable valid_domain not setted' unless ENV['OAUTH_VALID_DOMAIN']
-end
