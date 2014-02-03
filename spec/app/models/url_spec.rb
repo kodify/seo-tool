297e7dac2bad4bb423cc66b29f2cefa1241b6837
Adria Cidre <adria.cidre@gmail.com>
2014-02-03 12:19:27 +0100

    [#KOD-274] Extra tests

--- spec/app/models/url_spec.rb
+++ spec/app/models/url_spec.rb
@@ -2,23 +2,57 @@ require 'spec_helper'
 
 describe Url do
   describe '#original_domain' do
+    let!(:subdomain) { '' }
     let!(:high_level_domain) { 'com' }
-    let!(:url) { "http://www.supu.#{high_level_domain}" }
+    let!(:url) { "http://#{subdomain}supu.#{high_level_domain}" }
 
     before do
       subject.url = url
     end
 
-    describe 'one word domain' do
-      it 'should return one word domain' do
-        subject.original_domain.should eq 'supu.com'
+    describe 'for domains' do
+      describe 'one word domain' do
+        it 'should return one word domain' do
+          subject.original_domain.should eq 'supu.com'
+        end
+      end
+      describe 'two words domain' do
+        let!(:high_level_domain) { 'com.es' }
+        it 'should return two words domain' do
+          subject.original_domain.should eq 'supu.com.es'
+        end
       end
     end
-    describe 'two words domain' do
-      let!(:high_level_domain) { 'com.es' }
-      it 'should return two words domain' do
-        subject.original_domain.should eq 'supu.com.es'
+
+    describe 'for sub-domains' do
+      let!(:subdomain) { 'www.' }
+      describe 'one word domain' do
+        it 'should return one word domain' do
+          subject.original_domain.should eq 'supu.com'
+        end
+      end
+      describe 'two words domain' do
+        let!(:high_level_domain) { 'com.es' }
+        it 'should return two words domain' do
+          subject.original_domain.should eq 'supu.com.es'
+        end
       end
     end
+
+    describe 'for sub-domains' do
+      let!(:subdomain) { 'tupu.tamadre.' }
+      describe 'one word domain' do
+        it 'should return one word domain' do
+          subject.original_domain.should eq 'supu.com'
+        end
+      end
+      describe 'two words domain' do
+        let!(:high_level_domain) { 'com.es' }
+        it 'should return two words domain' do
+          subject.original_domain.should eq 'supu.com.es'
+        end
+      end
+    end
+
   end
 end
